1f695ee390e657abb0c29d495d6219409528813b
Victoria Guarnieri <84190435+vickyguar@users.noreply.github.com>
2023-06-22 08:50:59 -0300

    Delete COSAS QUE FUNCIONAN directory

--- COSAS QUE FUNCIONAN/Triggers/AnioLanzamientoSerieInvalido.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `serie_BEFORE_INSERT` BEFORE INSERT ON `serie` FOR EACH ROW BEGIN
-IF NEW.AnioLanzamiento > YEAR(CURDATE()) THEN
-	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Año de lanzamiento inválido';
-ELSE
-	INSERT INTO serie (NombreSerie, TiIngles, TiEspañol, AnioLanzamiento,Productor, CalifSalida)
-	VALUES (NEW.NombreSerie, NEW.TiIngles, NEW.TiEspañol, NEW.AnioLanzamiento,NEW.Productor, NEW.CalifSalida);
-END IF;
-END
