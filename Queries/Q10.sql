1a2a86b14fcaae375c75e4fa38dbc7a9af66ed43
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2023-06-22 01:01:42 -0300

    Add files via upload

---
+++ Queries/Q10.sql
@@ -0,0 +1,8 @@
+-- 10 Marcas de televisores más usados para casting
+SELECT MarcaDisp, COUNT(*) AS cantidad
+FROM dispositivo
+INNER JOIN casting ON dispositivo.idDispositivo = casting.DispReceptor
+WHERE dispositivo.TipoDisp LIKE 'televisor'
+GROUP BY MarcaDisp
+ORDER BY cantidad DESC
+LIMIT 10;
