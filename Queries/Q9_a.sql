44c3b304bc175ebb8ba21d5129e0ea0d6f9e3ed4
Victoria Guarnieri <84190435+vickyguar@users.noreply.github.com>
2023-06-22 00:44:42 -0300

    Add files via upload

---
+++ Queries/Q9_a.sql
@@ -0,0 +1,18 @@
+-- Listar cuál es el contenido más visto en watchparty para segmento entre 14 y 19 años
+SELECT *
+FROM 
+(
+(SELECT TiOriginal, usuario.Username, usuario.FechaNac FROM 
+watchparty 
+INNER JOIN reproduccionencurso ON watchparty.idReproduccion = reproduccionencurso.idReproduccion
+INNER JOIN usuario ON usuario.Username = watchparty.UserEmisor)
+
+UNION
+
+(SELECT TiOriginal, usuario.Username, usuario.FechaNac FROM 
+watchparty 
+INNER JOIN reproduccionencurso ON watchparty.idReproduccion = reproduccionencurso.idReproduccion
+INNER JOIN usuario ON usuario.Username = watchparty.UserReceptor)
+)
+WHERE(YEAR(CURDATE() - FechaNac) BETWEEN 14 AND 19)
+
