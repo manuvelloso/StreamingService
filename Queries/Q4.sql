6238b215486813deba7e8b98a46ea91000bd3fd0
Victoria Guarnieri <84190435+vickyguar@users.noreply.github.com>
2023-06-22 00:13:16 -0300

    Update Q4.sql

--- Queries/Q4.sql
+++ Queries/Q4.sql
@@ -8,11 +8,11 @@ FROM (
         capitulo.NombreSerie AS Serie
     FROM 
         capitulo
-        INNER JOIN historial ON capitulo.TiOriginal = historial.TiOriginal
-        INNER JOIN usuario ON historial.Username = usuario.Username 
+        INNER JOIN reproduccionencurso ON capitulo.TiOriginal = reproduccionencurso.TiOriginal
+        INNER JOIN usuario ON reproduccionencurso.Username = usuario.Username 
     WHERE 
         YEAR(usuario.FechaNac) BETWEEN YEAR(CURDATE()) - 35 AND YEAR(CURDATE()) - 25
-    GROUP BY capitulo.NombreSerie, historial.Username
+    GROUP BY capitulo.NombreSerie, reproduccionencurso.Username
 ) AS Reducida
 GROUP BY Serie
 ORDER BY Vistas DESC
