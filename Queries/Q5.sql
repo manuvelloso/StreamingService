f1c5a6e9b606be741a0a86be352f7b604b7e6ced
Victoria Guarnieri <84190435+vickyguar@users.noreply.github.com>
2023-06-22 00:14:59 -0300

    Update Q5.sql

--- Queries/Q5.sql
+++ Queries/Q5.sql
@@ -3,7 +3,7 @@
 SELECT 
 	TiOriginal AS Documentales
 FROM 
-	documental
+	documental INNER JOIN contenido ON documental.TiOriginal = contenido.TiOriginal
 WHERE 
 	AnioLanzamiento BETWEEN 2020 AND 2022 AND Tema LIKE "%COVID%" -- YEAR(AnioLanzamiento) BETWEEEN 2020 AND 2022 
 
